ed7bbb91607002e8ab788a84aab580df297bcd0b
Kazuya Yokoyama <yk3kzy@gmail.com>
2025-04-13 06:57:22 +0900

    プロジェクトの設定ファイルを作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import type { FC } from "react";
-
-const Home: FC = () => {
-	return (
-		<main>
-			<div className="flex justify-center items-center h-screen">
-				<div className="flex flex-col items-center">
-					<h1 className="text-2xl font-bold">Hello World</h1>
-				</div>
-			</div>
-		</main>
-	);
-};
-
-export default Home;
+export default function Home() {
+  return (
+    <main>
+      <div className="flex h-screen items-center justify-center">
+        <div className="flex flex-col items-center">
+          <h1 className="text-2xl font-bold">Hello World</h1>
+        </div>
+      </div>
+    </main>
+  );
+}
