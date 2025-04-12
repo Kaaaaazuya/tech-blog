388aa80520e446986504eb4abc77185f031fb626
Kazuya Yokoyama <yk3kzy@gmail.com>
2025-04-13 07:05:01 +0900

    T3 Env を設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { env } from "@/env";
+
 export default function Home() {
+  const url = env.DEBUG_URL;
+
   return (
     <main>
       <div className="flex h-screen items-center justify-center">
         <div className="flex flex-col items-center">
           <h1 className="text-2xl font-bold">Hello World</h1>
+          <p className="text-sm text-gray-500">{url}</p>
         </div>
       </div>
     </main>
